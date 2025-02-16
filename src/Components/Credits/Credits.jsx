16806d09fe82a49731588d9aff0e9e35d2e0a778
Andreea N <triplecutepnl@gmail.com>
2025-02-16 18:35:11 +0200

    small changes to credits

--- src/Components/Credits/Credits.jsx
+++ src/Components/Credits/Credits.jsx
@@ -4,7 +4,7 @@ import './Credits.css';
 export const CreditsBar = () => {
   return (
     <div className='credits-bar'>
-      <p>© 2024 Nedelcu Andreea | <a href="https://github.com/andreea-35">Github</a> | <a href="https://github.com/lukePeavey/quotable">using Quotable API</a> </p>
+      <p>© 2024 Nedelcu Andreea | <a href="https://github.com/andreea-35">Github</a> | using <a href="https://github.com/lukePeavey/quotable"> Quotable API</a> </p>
       </div>
   )
 }
